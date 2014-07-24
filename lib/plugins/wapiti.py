249507c04d4189352dcd1d54ad3b4b4275cae724
David Williams <david.williams2@digital.cabinet-office.gov.uk>
2014-07-24 15:09:18 +0100

    Added plugin loader and Watipi plugin

---
+++ lib/plugins/wapiti.py
@@ -0,0 +1,34 @@
+"""Plugin that wraps wapiti.
+"""
+
+from ..plugin import Plugin
+
+
+class Wapiti(Plugin):
+    """Wapiti is a web app scanning tool that searches for numerous
+    vulnerabilities.
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def run(self, url, checkers, output, output_format, auth):
+        if not isinstance(checkers, list):
+            checkers = [checkers]
+
+        if 'xss' in checkers:
+            checkers.append('permanentxss')
+
+        checkers.extend(['nikto', 'htaccess', 'crlf'])
+
+        try:
+            checker_liststr = ','.join(checkers)
+            cmd = ['wapiti', url, '-m', '-all,' + checker_liststr, '--format',
+                   output_format, '-o', output]
+            if auth is not None:
+                cmd.append('--auth', auth)
+            print(cmd)
+            stdout = self.__exec_process__(cmd)
+            return stdout
+        except Exception:
+            raise
--- sectool.py
+++ sectool.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import time
 import datetime
 from argh import arg, dispatch, set_default_command
 from argparse import ArgumentParser, ArgumentError
+from lib.plugin_loader import PluginLoader
 
-PLUGINS = ['wapiti']
-CHECKERS = ['xss', 'sql']
+PLUGIN_LOADER = PluginLoader()
+PLUGINS = PLUGIN_LOADER.plugins
+CHECKERS = ['xss', 'sql', 'backup', 'file', 'exec']
 FORMAT = ['json']
-OUTPUT_FILE = "sectool-report-{0}-{1}"
+OUTPUT_FILE = "sectool-report-{0}-{1}.{2}"
 
 
 @arg('url', type=str, help="URL to test against.")
@@ -19,18 +22,33 @@ OUTPUT_FILE = "sectool-report-{0}-{1}"
 @arg('--output', help="Output file name.", type=str)
 @arg('--format', choices=FORMAT, help='Format type to use for output',
      type=str)
-def sectool(url, email, plugins=PLUGINS, checkers=CHECKERS, output=None,
-            format=FORMAT):
+@arg('--auth', help="Credentials to use (auth%password)", type=str)
+def sectool(url, email, plugins=PLUGINS, checkers=CHECKERS[0:2], output=None,
+            format=FORMAT[0], auth=None):
     """Run security plugins to check for vulnerabilities in web applications.
     This tool is designed to be integrated inside of a Continuous Integration
     pipeline.
     """
+
     if url is None or email is None:
         raise ArgumentError('url and email must both be specified')
+
+    if auth is not None and len(auth.split("%")) > 2:
+        print("auth must have the format username%password")
+
     if output is None:
-        output = OUTPUT_FILE.format('$'.join(plugins),
-                                    datetime.datetime.now())
-    print(output)
+        date_frmt = "%y-%m-%d-%H-%M"
+        current_date = datetime.datetime.now().strftime(date_frmt)
+        output = OUTPUT_FILE.format('{0}{1}'.join(plugins),
+                                    current_date,
+                                    format)
+    for i in plugins:
+        instance = PLUGIN_LOADER.load_plugin(i)
+        t0 = time.time()
+        res = instance.run(url, checkers, output, format, auth)
+        t1 = time.time()
+        print("TIME TAKEN: ", (t1 - t0) / 60)
+        print(res)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
