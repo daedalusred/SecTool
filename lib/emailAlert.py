783250f8a4d79deffe432ca07452c91aae486f58
Peter McNerny <petermcnerny@gds3589.local>
2014-07-24 12:52:12 +0100

    Refactored email sending

--- lib/emailAlert.py
+++ lib/emailAlert.py
@@ -1,23 +1,35 @@
 """
 SecTool
 
-Send an email alert to the user which triggered the job containing the output from the SecTool run.
+Send the output from the plugins in an email alert to the user
+who triggered the job.
 
 23/07/2014
 """
 
 # https://docs.python.org/2/library/email-examples.html
 
+import datetime
 import smtplib
 from email.mime.text import MIMEText
 
 
-def getCommandLineOptions():
-    # TODO: get the command line options
-    me = "peter.mcnerny@digital.cabinet-office.gov.uk"  # TODO: set this value
-    you = "peter.mcnerny@digital.cabinet-office.gov.uk"  # TODO: get this from the cmd line options
-    pluginName = None  # TODO: get this from the cmd line options
-    return pluginName, me, you
+###############################################################################
+# Members
+###############################################################################
+
+FROM_ADDRESS = "noreply@digital.cabinet-office.gov.uk"
+SMTP_SERVER = "smtp.gmail.com"
+GOOGLE_APP_PASSWORD = "fnunearexavcvklx"
+
+command_line_args = None  # TODO: set by constructor
+jsonOutputFromTool = None  # TODO: set by constructor
+pluginName = "Wapiti"   # TODO: set by constructor
+usersEmailAddress = "peter.mcnerny@digital.cabinet-office.gov.uk"  # TODO: set by constructor
+
+###############################################################################
+# Functions
+###############################################################################
 
 
 def getOutputFromJsonObject():
@@ -25,49 +37,54 @@ def getOutputFromJsonObject():
     # TODO: parse the JSON object containing the output
     #
 
-    #
-    # TODO: update the output variable which is a placeholder for the parsed JSON output
-    #
-    output = None
-    return output
+    numOfErrors = 1  # TODO: set this variable
+
+    output = None  # TODO: set this variable
+    return output, numOfErrors
 
 
-def createEmail(output):
-    # Create a text/plain message
-    msg = MIMEText(getOutputFromJsonObject().read())
+def createEmail():
+    # create the contents of the email
+    output = getOutputFromJsonObject()
 
-    # get the command line options
-    options = getCommandLineOptions()
+    #message = MIMEText(output[0].read())  # TODO: use the JSON output
+    message = MIMEText("This is a text message")  # TODO: this is test data - remove
 
-    msg['Subject'] = "SecTool Results: %s" % options[0]
-    msg['From'] = options[1]  # sender's address
-    msg['To'] = options[2]  # user's email address
+    # how many issues the tool has found
+    numOfErros = output[1]
+    issues = "Issues"
+    if numOfErros == 1:
+        issues = "Issue"
+
+    # add addressing to the email
+    date = datetime.datetime.now().strftime("%y-%m-%d %H:%M")
+    message['Subject'] = "SecTool Results: {0} - {1} {2} [{3}]".format(
+        pluginName, numOfErros, issues)
+    message['from'] = FROM_ADDRESS
+    message['to'] = usersEmailAddress  # email address of user who ran the tool
+    return message
 
 
-def sendEmail(me, you, msg):
-    # Send the email via the SMTP server
-    s = smtplib.SMTP('smtp.gmail.com')
-    s.sendmail(me, [you], msg)
-    s.quit
+def sendEmail(message):
+    srv = smtplib.SMTP(SMTP_SERVER)
+    srv.ehlo()
+    srv.starttls()
+    srv.ehlo()
+    srv.login(usersEmailAddress, GOOGLE_APP_PASSWORD)
+    srv.sendmail(FROM_ADDRESS, usersEmailAddress, message.as_string())
+    srv.close()
 
 
+###############################################################################
+# Class
+###############################################################################
 class Email:
     pass
 
 
-#
-# TESTING
-#
-addr = "peter.mcnerny@digital.cabinet-office.gov.uk"
-message = MIMEText("This is a text message")
-message['Subject'] = "Test"
-message['from'] = "noreply@digital.cabinet-office.gov.uk"
-message['to'] = addr
 
-srv = smtplib.SMTP("smtp.gmail.com")
-srv.ehlo()
-srv.starttls()
-srv.ehlo()
-srv.login(addr, "fnunearexavcvklx")
-srv.sendmail(addr, addr, message.as_string())
-srv.close()
+###############################################################################
+# Testing
+###############################################################################
+msg = createEmail()
+sendEmail(msg)
