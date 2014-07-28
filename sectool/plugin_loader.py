0e7d64c68336e992353bb570b95604b406f05e34
David Williams <david.williams2@digital.cabinet-office.gov.uk>
2014-07-28 09:11:00 +0100

    Made exceptions more fine grained in the plugin loader

--- sectool/plugin_loader.py
+++ sectool/plugin_loader.py
@@ -6,6 +6,7 @@ assert wapiti  # Silence unused import warnings
 from os.path import splitext
 from os import listdir
 import logging
+import errno
 
 PLUGIN_LOC = "sectool/plugins"  # Location of Plugin Dir.
 FILE_NAME_BLACKLIST = ["__init__.py", "__pycache__"]  # Ignore these files
@@ -23,13 +24,20 @@ class PluginLoader(object):
                             splitext(x)[1] not in FILE_EXT_BLACKLIST]
             logging.info("Found plugins {0}".format(' '.join(self.plugins)))
 
-        except Exception:
-            logging.error("Failed to list directory {0}".format(PLUGIN_LOC))
+        except (OSError, IOError) as e:
+            if e.errno == errno.ENOENT:
+                msg = "Failed to listdir for plugin {0} with {1}"
+                msg = msg.format(PLUGIN_LOC, e.errno)
+                logging.error(msg)
+            else:
+                msg = "Failed to listdir for plugin {0} with {1}"
+                msg = msg.format(PLUGIN_LOC, e.errno)
+                logging.error(msg)
             raise
 
     def load_plugin(self, plugin_name):
         """Load a plugin from the plugins directory with the name set in
-        plugin_name. If an instance can't be found raise the exception.
+        plugin_name. If an instance can't be found raise that exception.
         """
         try:
             logging.info("Getting plugin {0}".format(plugin_name))
@@ -37,6 +45,6 @@ class PluginLoader(object):
                                                     plugin_name.capitalize()))
             plugin_instance = plugin_instance(plugin_name)
             return plugin_instance
-        except Exception:
+        except NameError:
             logging.error("failed getting plugin {0}".format(plugin_name))
             raise
